361e658ee13d7a26c415217afebe39d3d99d3065
Marcos <marcossosa21415@gmail.com>
2023-11-10 21:40:16 -0300

    Register.js useState()

--- gdtms_frontend/src/componentes/Register.js
+++ gdtms_frontend/src/componentes/Register.js
@@ -1,38 +1,58 @@
+import { useState} from "react";
+
 export function Register({handleForm}) {
 
-  const enviarDatos = (e) => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    pais: "",
+    password: "",
+    confPassword: ""
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormData({
+      ...formData,
+      [name]: value
+    }
+    )
+  }
+
+  const consoleDatos = (e) => {
     e.preventDefault();
-  };
+    console.log("Datos del form:", formData)
+  }
 
   return (
     <div className="fondoFormulario col cen">
       <div className="contenedorFormulario register col">
         <h1>Registro</h1>
-        <form className="col">
+        <form className="col" onSubmit={consoleDatos}>
           <label className="col">
             Username:
-            <input type="text" />
+            <input type="text" name="username" value={formData.username} onChange={handleInputChange} />
           </label>
           <label className="col">
             E-Mail:
-            <input type="text" />
+            <input type="text" name="email" value={formData.email} onChange={handleInputChange}/>
           </label>
           <label className="col">
             Pais:
-            <select>
-              <option>Afganistan</option>
-              <option>Argentina</option>
+            <select name="pais" value={formData.pais} onChange={handleInputChange}>
+              <option value="Afganistan" >Afganistan</option>
+              <option value="Argentina" >Argentina</option>
             </select>
           </label>
           <label className="col">
             Contraseña:
-            <input type="password" />
+            <input type="password" name="password" value={formData.password} onChange={handleInputChange} />
           </label>
           <label className="col">
             Confirmar contraseña:
-            <input type="password" />
+            <input type="password" name="confPassword" value={formData.confPassword} onChange={handleInputChange}/>
           </label>
-          <button type="submit" className="btn" onSubmit={enviarDatos}>
+          <button type="submit" className="btn" >
             CREAR CUENTA
           </button>
           <span className="cambiarForm" onClick={handleForm}>Iniciar sesion</span>
